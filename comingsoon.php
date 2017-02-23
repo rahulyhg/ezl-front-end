26b691ba20537b6ac91170079514f48ed79ea12b
Skillz4Killz <admin@ezlgg.com>
2017-02-23 14:17:14 -0500

    coming soon splash page, still needs a cool countdown timer and css. this is just the basic template

---
+++ comingsoon.php
@@ -0,0 +1,11 @@
+<?php include 'defaulthead.html';?>
+  <body>
+    <?php include 'authenticatednavbar.html';?>
+    
+<h2>Coming Soon!</h2>
+
+<p>Tell us what you think is going to be here and what you want to see here at <a href="https://discord.me/ezl"></a></p>
+
+    <?php include 'foundationscript.html';?>
+  </body>
+</html>
