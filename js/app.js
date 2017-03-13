46a5289eb378992c50997c119c6fef22e3f49d6e
ShaDaz <shadaz@aquarosgaming.com>
2017-03-12 22:39:31 -0500

    Added in the rest of the pages and routes

--- js/app.js
+++ js/app.js
@@ -1,104 +1,176 @@
 var app = angular.module("ezlApp", ["ngRoute"]);
 
 // Delayed Foundation Reinitialization
-app.run(function($timeout){
-    $timeout(function() {
-        $(document).foundation();
-    }, 100);
+app.run(function ($timeout) {
+  $timeout(function () {
+    $(document).foundation();
+  }, 100);
 });
 
 // navbar
-(function($) {
-  $(function() {
+(function ($) {
+  $(function () {
     // If link has dropdown, add submenu toggle
-    $('nav ul li a:not(:only-child)').hover(function(i) {
+    $('nav ul li a:not(:only-child)').hover(function (i) {
       $(this).siblings('.nav-dropdown').show();
       // Close one dropdown when selecting another
       $('.nav-dropdown').not($(this).siblings()).hide();
       i.stopPropagation();
     });
     // Clicking html will remove the dropdown class
-    $('html').click(function() {
+    $('html').click(function () {
       $('.nav-dropdown').hide();
     });
     // Toggle open and close nav styles on click
-    $('#nav-toggle').click(function() {
+    $('#nav-toggle').click(function () {
       $('nav ul').slideToggle();
     });
     // Hamburger toggle
-    $('#nav-toggle').on('click', function() {
+    $('#nav-toggle').on('click', function () {
       this.classList.toggle('active');
     });
   });
 })(jQuery);
 
-app.config(['$routeProvider', function($routeProvider) {
-   $routeProvider
+app.config(['$routeProvider', function ($routeProvider) {
+  $routeProvider
 
-      .when('/', {
-         templateUrl : './pages/home.html',
-         controller : 'mainController'
-      })
+    .when('/', {
+      templateUrl: './pages/home.html',
+      controller: 'mainController'
+    })
 
-      .when('/news', {
-         templateUrl : './pages/news.html',
-         controller : 'newsController'
-      })
+    .when('/news', {
+      templateUrl: './pages/news.html',
+      controller: 'newsController'
+    })
 
-      // .when('/fantasy', {
-      //    templateUrl : './pages/fantasy.html',
-      //    controller : 'fantasyController'
-      // })
+    .when('/stats', {
+      templateUrl: './pages/stats/stats.html',
+      controller: 'statsController'
+    })
 
-      .when('/roster', {
-         templateUrl : './pages/roster.html',
-         controller : 'rosterController'
-      });
+    .when('/scoreboard', {
+      templateUrl: './pages/stats/scoreboard.html',
+      controller: 'scoreboardController'
+    })
+
+    .when('/schedule', {
+      templateUrl: './pages/stats/schedule.html',
+      controller: 'scheduleController'
+    })
+
+    .when('/standings', {
+      templateUrl: './pages/stats/standings.html',
+      controller: 'standingsController'
+    })
+
+    .when('/roster', {
+      templateUrl: './pages/roster.html',
+      controller: 'rosterController'
+    })
+
+    .when('/sponsors', {
+      templateUrl: './pages/sponsors.html',
+      controller: 'sponsorsController'
+    })
+
+    .when('/user_profile', {
+      templateUrl: './pages/profile/user_profile.html',
+      controller: 'user_profileController'
+    })
+
+    .when('/player_stats', {
+      templateUrl: './pages/profile/player_stats.html',
+      controller: 'player_statsController'
+    })
+
+    .when('/apply', {
+      templateUrl: './pages/profile/apply.html',
+      controller: 'applyController'
+    })
+
+    .when('/settings', {
+      templateUrl: './pages/profile/settings.html',
+      controller: 'settingsController'
+    });
 }]);
 
-app.controller('mainController', function($scope) {
-   $scope.message = 'Main Controller is working!';
-   $scope.bShow = false;
+app.controller('mainController', function ($scope) {
+  $scope.message = 'Main Controller is working!';
+  $scope.bShow = false;
 });
 
-app.controller('newsController', function($scope) {
-   $scope.message = 'news controller is working';
+app.controller('newsController', function ($scope) {
+  $scope.message = 'news controller is working';
 });
 
-app.controller('fantasyController', function($scope) {
-   $scope.message = 'fantasy controller is working';
+app.controller('statsController', function ($scope) {
+  $scope.message = 'Stats controller is working';
 });
 
-app.controller('rosterController', function($scope, $http) {
-   $scope.message = 'roster controller is working';
+app.controller('scoreboardController', function ($scope) {
+  $scope.message = 'Scoreboard controller is working';
+});
 
-   $scope.FullRoster = null; //Initialization, important to maintain scope
+app.controller('scheduleController', function ($scope) {
+  $scope.message = 'Schedule controller is working';
+});
 
-   $http({
-         method: 'GET',
-         url: 'http://173.80.170.125:8000/data/proteams'
-   }).then(function success(res) {
-         $scope.FullRoster = res.data; //Declaration
-         //alert(JSON.stringify($scope.FullRoster));
-   }, function failure(res) {
-         alert("HTTP failed!");
-   });
+app.controller('standingsController', function ($scope) {
+  $scope.message = 'Standings controller is working';
+});
+
+app.controller('rosterController', function ($scope, $http) {
+  $scope.message = 'roster controller is working';
+
+  $scope.FullRoster = null; //Initialization, important to maintain scope
+
+  $http({
+    method: 'GET',
+    url: 'http://173.80.170.125:8000/data/proteams'
+  }).then(function success(res) {
+    $scope.FullRoster = res.data; //Declaration
+    //alert(JSON.stringify($scope.FullRoster));
+  }, function failure(res) {
+    alert("HTTP failed!");
+  });
 
 
 
 
 });
 
-app.directive("mainNavBar", function() {
-   return {
-      restrict: "AECM",
-      templateUrl: "./include/header.html"
-   };
+app.controller('sponsorsController', function ($scope) {
+  $scope.message = 'Sponsors controller is working';
 });
 
-app.directive("mainFooter", function() {
-   return {
-      restrict: "AECM",
-      templateUrl: "./include/footer.html"
-   };
+app.controller('user_profileController', function ($scope) {
+  $scope.message = 'user_profile controller is working';
+});
+
+app.controller('player_statsController', function ($scope) {
+  $scope.message = 'player_stats controller is working';
+});
+
+app.controller('applyController', function ($scope) {
+  $scope.message = 'apply controller is working';
+});
+
+app.controller('settingsController', function ($scope) {
+  $scope.message = 'settings controller is working';
+});
+
+app.directive("mainNavBar", function () {
+  return {
+    restrict: "AECM",
+    templateUrl: "./include/header.html"
+  };
+});
+
+app.directive("mainFooter", function () {
+  return {
+    restrict: "AECM",
+    templateUrl: "./include/footer.html"
+  };
 });
