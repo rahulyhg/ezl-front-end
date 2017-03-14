256d2d8100a5bae16febc2f6a4754def5b73a6f6
r3wt <artagious.one@gmail.com>
2017-03-13 20:33:13 -0700

    implemented navbar into db_header, fantasy_header & header.html/CSS changes/navbar jQuery fixed

--- js/fantasy.js
+++ js/fantasy.js
@@ -1,3 +1,28 @@
+// navbar
+(function ($) {
+  $(function () {
+    // If link has dropdown, add submenu toggle
+    $('nav ul li a:not(:only-child)').hover(function (i) {
+      $(this).siblings('.nav-dropdown').show();
+      // Close one dropdown when selecting another
+      $('.nav-dropdown').not($(this).siblings()).hide();
+      i.stopPropagation();
+    });
+    // Clicking html will remove the dropdown class
+    $('html').click(function () {
+      $('.nav-dropdown').hide();
+    });
+    // Toggle open and close nav styles on click
+    $('#nav-toggle').click(function () {
+      $('nav ul').slideToggle();
+    });
+    // Hamburger toggle
+    $('#nav-toggle').on('click', function () {
+      this.classList.toggle('active');
+    });
+  });
+})(jQuery);
+
 var fantasy_app = angular.module("fantasy", ["ngRoute"]);
 
 fantasy_app.run(function($timeout){
@@ -32,7 +57,7 @@ fantasy_app.controller('fantasy_joinController', function($scope) {
 fantasy_app.directive("fantasyNavBar", function() {
    return {
       restrict: "AECM",
-      templateUrl: "./include/fantasy_header.html" 
+      templateUrl: "./include/fantasy_header.html"
    };
 });
 
