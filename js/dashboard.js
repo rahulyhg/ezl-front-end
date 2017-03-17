a2d879df08da3d4c206e0706e2960403e69e8f99
r3wt <artagious.one@gmail.com>
2017-03-16 20:32:41 -0700

    foundation will now reinitialize automatically when new content is loaded / integrated jquery into navbar directives

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,34 +1,12 @@
-// navbar
-(function ($) {
-  $(function () {
-    // If link has dropdown, add submenu toggle
-    $('nav ul li a:not(:only-child)').hover(function (i) {
-      $(this).siblings('.nav-dropdown').show();
-      // Close one dropdown when selecting another
-      $('.nav-dropdown').not($(this).siblings()).hide();
-      i.stopPropagation();
-    });
-    // Clicking html will remove the dropdown class
-    $('html').click(function () {
-      $('.nav-dropdown').hide();
-    });
-    // Toggle open and close nav styles on click
-    $('#nav-toggle').click(function () {
-      $('nav ul').slideToggle();
-    });
-    // Hamburger toggle
-    $('#nav-toggle').on('click', function () {
-      this.classList.toggle('active');
-    });
-  });
-})(jQuery);
-
 var db_app = angular.module("dashboard", ["ngRoute"]);
 
-db_app.run(function($timeout){
-    $timeout(function() {
-        $(document).foundation();
-    }, 100);
+// Delayed Foundation Reinitialization
+db_app.run(function($rootScope, $timeout) {
+     $rootScope.$on('$viewContentLoaded', function () {
+            $timeout(function(){
+              $(document).foundation();
+            },300)
+        });
 });
 
 db_app.config(['$routeProvider', function($routeProvider) {
@@ -57,8 +35,33 @@ db_app.controller('db_new_postController', function($scope) {
 db_app.directive("dbNavBar", function() {
    return {
       restrict: "AECM",
-      templateUrl: "./include/db_header.html"
+      templateUrl: "./include/db_header.html",
+      link: link
    };
+
+   function link($scope, $element, $attr){
+      (function ($) {
+        // If link has dropdown, add submenu toggle
+        $('nav ul li a:not(:only-child)').hover(function (i) {
+          $(this).siblings('.nav-dropdown').show();
+          // Close one dropdown when selecting another
+          $('.nav-dropdown').not($(this).siblings()).hide();
+          i.stopPropagation();
+        });
+        // Clicking html will remove the dropdown class
+        $('html').click(function () {
+          $('.nav-dropdown').hide();
+        });
+        // Toggle open and close nav styles on click
+        $('#nav-toggle').click(function () {
+          $('nav ul').slideToggle();
+        });
+        // Hamburger toggle
+        $('#nav-toggle').on('click', function () {
+          this.classList.toggle('active');
+        });
+      }(jQuery));
+  };
 });
 
 db_app.directive("dbFooter", function() {
