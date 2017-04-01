3506bb409475a61bd7a56123388d75dd2cfe7e89
Raj Baweja <yuvi518@yahoo.com>
2017-04-01 13:23:37 -0500

    Added fantasy_createController for the fantasy_create page

--- js/fantasy_controllers.js
+++ js/fantasy_controllers.js
@@ -25,7 +25,30 @@ fantasy_app.controller('fantasy_roomController', function ($scope) {
   });
 
 });
+fantasy_app.controller('fantasy_createController', function ($scope) {
+	this.create = function(product) {
+		this.fantasy = {
+			$scope.name,
+			$scope.amtOfOwners,
+			$scope.NA,
+			$scope.EU,
+			$scope.SEA,
+			$scope.EA
+    };
 
+
+  $http({
+    method: 'POST',
+    url: 'http://173.80.170.125:8000/data/proteams',
+	data:this.fantasy
+  }).then(function success(res) {
+    alert("Created!");
+	fantasy = {};
+  }, function failure(res) {
+    alert("HTTP failed!");
+  });
+
+});
 fantasy_app.controller('fantasy_joinController', function ($scope) {
   $scope.message = 'fantasy_join controller is working';
 });
